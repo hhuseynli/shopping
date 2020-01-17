2239f28a2f72053e2c13e3759a0cd9d0a84af377
hhuseynli <huseyn00h1843@gmail.com>
2020-01-17 19:36:27 +0400

    LOMBOK
    I configured lombok library for my project.

--- back/shopping/src/main/java/com/Huseyn/shopping/model/Category.java
+++ back/shopping/src/main/java/com/Huseyn/shopping/model/Category.java
@@ -1,25 +1,16 @@
 package com.Huseyn.shopping.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Category {
 	private Integer id;
 	private String name;
 	
-	@Override
-	public String toString() {
-		return "Category [id=" + id + ", name=" + name + "]";
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
 	
 	
 }
--- back/shopping/src/main/java/com/Huseyn/shopping/model/Product.java
+++ back/shopping/src/main/java/com/Huseyn/shopping/model/Product.java
@@ -2,6 +2,12 @@ package com.Huseyn.shopping.model;
 
 import java.io.File;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
 public class Product {
 		private Integer id;
 		private String name;
@@ -13,66 +19,7 @@ public class Product {
 		private String seller;
 		private String phone;
 		private String email;
-		public Integer getId() {
-			return id;
-		}
-		public void setId(Integer id) {
-			this.id = id;
-		}
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public String getDescription() {
-			return description;
-		}
-		public void setDescription(String description) {
-			this.description = description;
-		}
-		public File getImage() {
-			return image;
-		}
-		public void setImage(File image) {
-			this.image = image;
-		}
-		public Integer getPrice() {
-			return price;
-		}
-		public void setPrice(Integer price) {
-			this.price = price;
-		}
-		public Category getCategory() {
-			return category;
-		}
-		public void setCategory(Category category) {
-			this.category = category;
-		}
-		public String getCity() {
-			return city;
-		}
-		public void setCity(String city) {
-			this.city = city;
-		}
-		public String getSeller() {
-			return seller;
-		}
-		public void setSeller(String sellerName) {
-			this.seller = sellerName;
-		}
-		public String getPhone() {
-			return phone;
-		}
-		public void setPhone(String phone) {
-			this.phone = phone;
-		}
-		public String getEmail() {
-			return email;
-		}
-		public void setEmail(String email) {
-			this.email = email;
-		}
+		
 		
 		
 		} 
