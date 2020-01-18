3b83259d14eed04d5d6ddd123a34e4d5cf53d6f5
hhuseynli <huseyn00h1843@gmail.com>
2020-01-18 09:25:17 +0400

    Update User.java
    I changed User model for Jpa

--- back/shopping/src/main/java/com/Huseyn/shopping/model/User.java
+++ back/shopping/src/main/java/com/Huseyn/shopping/model/User.java
@@ -1,11 +1,18 @@
 package com.Huseyn.shopping.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
+@Table(name="users")
 public class User {
+	@Id
 private String username;
 private String password;
 private Byte enabled;
