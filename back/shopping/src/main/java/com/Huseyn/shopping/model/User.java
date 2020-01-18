af1c238ed667123e334ed6bf1362937eab403404
hhuseynli <huseyn00h1843@gmail.com>
2020-01-18 09:18:43 +0400

    Create User.java
    I created user model

---
+++ back/shopping/src/main/java/com/Huseyn/shopping/model/User.java
@@ -0,0 +1,13 @@
+package com.Huseyn.shopping.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+private String username;
+private String password;
+private Byte enabled;
+private String full_Name;
+}
