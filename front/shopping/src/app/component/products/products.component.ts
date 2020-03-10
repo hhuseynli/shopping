4fed04c450733eb63a646efd66c87680aae77de6
hhuseynli <huseyn00h1843@gmail.com>
2020-03-10 18:29:47 +0400

    fixing repetition problem

--- front/shopping/src/app/component/products/products.component.ts
+++ front/shopping/src/app/component/products/products.component.ts
@@ -14,50 +14,61 @@ import { BasketService } from 'src/app/service/basket.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  products:Product[]=[];
-  begin:number=0;
-  download:string=`${API_URL}/filedownload/files/`;
-  
-  constructor(private service:ProductService, private matDialog:MatDialog, private upload:UploadService, private basket:BasketService) { }
+  products: Product[] = [];
+  begin: number = 0;
+  download: string = `${API_URL}/filedownload/files/`;
+
+  constructor(private service: ProductService, private matDialog: MatDialog, private upload: UploadService, private basket: BasketService) { }
 
   ngOnInit() {
     this.service.findPartial(this.begin).subscribe(
-      resp=>{
-        this.products=resp;
+      resp => {
+        this.products = resp;
       }
     );
-    
-   
-  
+
+
+
   }
-  
-  onScroll(){
-    this.begin+=10;
+
+  onScroll() {
+    this.begin += 10;
     this.service.findPartial(this.begin).subscribe(
-      resp=>{
+      resp => {
         this.products.push(...resp);
       }
     );
 
   }
 
-  onProductSelected(product:Product){
-    this.service.product=product;
+  onProductSelected(product: Product) {
+    this.service.product = product;
     this.matDialog.open(ProductViewComponent);
 
   }
-  viewImage(image){
-    this.upload.image=image;
+  viewImage(image) {
+    this.upload.image = image;
     this.matDialog.open(ImageViewComponent);
   }
 
-  onCart(product:Product){
-    let orderP:OrderProduct = new OrderProduct();
-    orderP.product=product;
-    orderP.quantity=1;
-    this.basket.orderProducts.push(orderP);
+  onCart(product: Product) {
+    let alreadyInBasket: boolean = false;
+    for (let index = 0; index < this.basket.orderProducts.length; index++) {
+      const element = this.basket.orderProducts[index];
+      if (element.product.id == product.id) {
+        alreadyInBasket = true;
+        this.basket.orderProducts[index].quantity++;
+        break;
+      }
+    }
+    if (!alreadyInBasket) {
+      let orderP: OrderProduct = new OrderProduct();
+      orderP.product = product;
+      orderP.quantity = 1;
+      this.basket.orderProducts.push(orderP);
+    }
     this.basket.changeProductCount();
-    
+
 
 
   }
