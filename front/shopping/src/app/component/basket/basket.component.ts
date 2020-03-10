6380aa50db9de27dcb87d0c96170ead0b0adeac8
hhuseynli <huseyn00h1843@gmail.com>
2020-03-10 17:34:35 +0400

    developing view, finding total price and updating count when a product is deleted or quantity is changed

--- front/shopping/src/app/component/basket/basket.component.ts
+++ front/shopping/src/app/component/basket/basket.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { BasketService } from 'src/app/service/basket.service';
 import { OrderProduct, Product } from '../model/product';
 import { API_URL } from 'src/app/constants';
+import { UploadService } from 'src/app/service/upload.service';
+import { MatDialog } from '@angular/material';
+import { ImageViewComponent } from '../image-view/image-view.component';
+import { OrderConfirmationComponent } from '../order-confirmation/order-confirmation.component';
 
 @Component({
   selector: 'app-basket',
@@ -11,17 +15,41 @@ import { API_URL } from 'src/app/constants';
 export class BasketComponent implements OnInit {
   download: string = `${API_URL}/filedownload/files/`;
   products: OrderProduct[] = [];
-  constructor(public service: BasketService) { }
+  price:number=0;
+  constructor(public service: BasketService,private uploadService:UploadService, private matDialog:MatDialog) { }
 
   ngOnInit() {
-    this.service.orderProducts.forEach(element => {
-      console.log(element);
-    });
     this.products = this.service.orderProducts;
+    
+   this.service.totalPriceChanged.subscribe(
+      resp=>{
+        this.price=resp;
+      }
+    );
+    this.service.changeTotalPrice();
   }
-  onDelete(product: Product) {
-    let id: number = product.id;
-    this.products.splice(id, 1);
+  onDelete(count) {
+    this.products.splice(count, 1);
+    this.service.changeProductCount();
+    this.service.changeTotalPrice();
+  }
+
+  onProductQuantityChanged(product:OrderProduct, count:number){
+    let quantity=product.quantity;
+    if(quantity==0){
+      this.onDelete(count);
+    }else{
+    this.service.changeProductCount();
+    this.service.changeTotalPrice();
+    }
+  }
+  viewImage(image){
+    this.uploadService.image=image;
+    this.matDialog.open(ImageViewComponent);
+  }
+
+  onConfirmOrder(){
+    this.matDialog.open(OrderConfirmationComponent);
   }
 
 }
