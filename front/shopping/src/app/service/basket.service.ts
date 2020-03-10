8dde5a88448e1d1f5b5610f38105832f97191f8e
hhuseynli <huseyn00h1843@gmail.com>
2020-03-10 17:30:06 +0400

    creating methods that set product count and total price

--- front/shopping/src/app/service/basket.service.ts
+++ front/shopping/src/app/service/basket.service.ts
@@ -7,6 +7,28 @@ import { OrderProduct } from '../component/model/product';
 export class BasketService {
   productCountChanged:EventEmitter<number>=new EventEmitter();
   orderProducts:OrderProduct[]=[];
-  basketCount=0;
+  totalPriceChanged:EventEmitter<number>=new EventEmitter();
+
+  changeProductCount(){
+    let orderSum:number=0;
+    for (let index = 0; index < this.orderProducts.length; index++) {
+      const element = this.orderProducts[index];
+      orderSum+=element.quantity;
+    }
+    this.productCountChanged.emit(orderSum);
+  }
+
+  changeTotalPrice(){
+      let totalSum:number=0;
+      for (let index = 0; index < this.orderProducts.length; index++) {
+        const element = this.orderProducts[index];
+        totalSum+=(element.quantity*element.product.price);
+      }
+      this.totalPriceChanged.emit(totalSum);
+    
+  }
+
+
+
   constructor() { }
 }
